c9fa370a54acc63783a76f761a3a4a2711f1848b
Kayla Li <145611742+kayla177@users.noreply.github.com>
2024-11-26 18:22:53 -0500

    initial commit

---
+++ Edge.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Edge
+{
+private:
+    std::string sourceID;
+    std::string label;
+    std::string destinationID;
+    double weight;
+
+public:
+    Edge();
+};
---
+++ Node.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Node
+{
+private:
+    std::string ID;
+    std::string name;
+    std::string type;
+
+public:
+    Node();
+};
